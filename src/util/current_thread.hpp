f09144636d7c3695fc847faaf3e219fa98bf8d5c
czn <iamczn.cpp@gmail.com>
2018-05-03 22:30:14 +0800

    add

---
+++ src/util/current_thread.hpp
@@ -0,0 +1,21 @@
+﻿// Part of Asuka utility, internal header
+// Copyleft 2018, vczn
+
+#pragma once
+#ifndef ASUKA_CURRENT_THREAD_HPP
+#define ASUKA_CURRENT_THREAD_HPP
+
+#include <thread>
+
+namespace Asuka
+{
+
+inline std::thread::id current_thread_id()
+{
+    static thread_local std::thread::id id = std::this_thread::get_id();
+    return id;
+}
+
+} // namespace Asuka
+
+#endif // ASUKA_CURRENT_THREAD_HPP
