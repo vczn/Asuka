1dc5416807172e239db34dee2acb97d5e6e1d866
czn <iamczn.cpp@gmail.com>
2018-05-19 00:12:20 +0800

    add poller_base

---
+++ src/net/poller_base.cpp
@@ -0,0 +1,36 @@
+﻿#include "poller_base.hpp"
+
+#include "channel.hpp"
+#include "event_loop.hpp"
+
+namespace Asuka
+{
+
+namespace Net
+{
+
+PollerBase::PollerBase(EventLoop* loop)
+    : mLoop(loop)
+{
+}
+
+PollerBase::~PollerBase()
+{
+}
+
+bool PollerBase::has_channel(const Channel& channel) const
+{
+    assert_in_loop_thread();
+    auto iter = mChannels.find(channel.get_fd());
+
+    return iter != mChannels.end() && iter->second == &channel;
+}
+
+void PollerBase::assert_in_loop_thread() const
+{
+    return mLoop->assert_in_loop_thread();
+}
+
+} // namespace Net
+
+} // namespace Asuka
---
+++ src/net/poller_base.hpp
@@ -0,0 +1,67 @@
+﻿// Part of Asuka net utility, internal header
+// Copyleft 2018, vczn
+
+#pragma once
+#ifndef ASUKA_POLLER_BASE
+#define ASUKA_POLLER_BASE
+
+#include <map>
+#include <memory>
+#include <vector>
+
+#include "../util/noncopyable.hpp"
+#include "../util/time_stamp.hpp"
+
+namespace Asuka
+{
+
+namespace Net
+{
+
+class Channel;
+class EventLoop;
+
+// PollerBase is base class Poller and Epoller
+// it is for I/O mutiplexing
+class PollerBase : Noncopyable
+{
+public:
+    using ChannelList = std::vector<Channel*>;
+
+public:
+    PollerBase(EventLoop* loop);
+    virtual ~PollerBase();
+
+    // polls the I/O events
+    // must be called in the mLoop's thread
+    // `activeChannels` is a value-result
+    virtual TimeStamp poll(int timeoutMs, ChannelList& activeChannels) = 0;
+
+    // update the interested I/O events
+    // must be called in the mLoop's thread
+    virtual void update_channel(Channel& channel) = 0;
+
+    // remove the channel when it is destroyed
+    // must be called in the mLoop's thread
+    virtual void remove_channel(Channel& channel) = 0;
+
+    bool has_channel(const Channel& channel) const;
+
+    static std::unique_ptr<PollerBase> create_default_poller(EventLoop* loop);
+
+    void assert_in_loop_thread() const;
+
+protected:
+    // <fd, Channel*>
+    using ChannelMap = std::map<int, Channel*>;
+    ChannelMap mChannels;
+
+private:
+    EventLoop* mLoop;
+};
+
+} // namespace Net
+
+} // namespace Asuka
+
+#endif // ASUKA_POLLER_BASE
