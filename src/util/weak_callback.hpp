1422d4d7c8f4fb4347e01cb0285074032502de42
czn <iamczn.cpp@gmail.com>
2018-05-28 22:49:41 +0800

    add weak_callback

---
+++ src/util/weak_callback.hpp
@@ -0,0 +1,53 @@
+﻿// Part of Asuka utility, internal header
+// Copyleft 2018, vczn
+
+#pragma once
+#ifndef ASUKA_WEAK_CALLBACK_HPP
+#define ASUKA_WEAK_CALLBACK_HPP
+
+#include <memory>
+
+namespace Asuka
+{
+
+template <typename T, typename... Args>
+class WeakCallback
+{
+public:
+    WeakCallback(const std::weak_ptr<T>& obj,
+        std::function<void(T*, Args...)> function)
+        : mObject(obj),
+          mFunction(std::move(function))
+    {
+    }
+
+    void operator()(Args&&... args) const
+    {
+        std::shared_ptr<T> sp(mObject.lock());
+        if (sp)
+        {
+            mFunction(sp.get(), std::forward<Args>(args)...);
+        }
+    }
+private:
+    std::weak_ptr<T> mObject;
+    std::function<void(T*, Args...)> mFunction;
+};
+
+template <typename T, typename... Args>
+WeakCallback<T, Args...> make_weak_callback(const std::shared_ptr<T>& obj,
+    void (T::*function)(Args...))
+{
+    return WeakCallback<T, Args...>(obj, function);
+}
+
+template <typename T, typename... Args>
+WeakCallback<T, Args...> make_weak_callback(const std::shared_ptr<T>& obj,
+    void(T::*function)(Args...) const)
+{
+    return WeakCallback<T, Args...>(obj, function);
+}
+
+} // namespace Asuka
+
+#endif // ASUKA_WEAK_CALLBACK_HPP
